145c08fb54b4ad92c4d4a36c7f7028969245958b
Lourguioui <iz_lourguioui@esi.dz>
2020-07-09 05:19:26 +0200

    add actions

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -9,22 +9,29 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faTimes, faArrowRight, faFlag } from "@fortawesome/free-solid-svg-icons";
 
 export default class TodoItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
     render() {
+
         return (
             <Row className="todo-item-row" >
                 <Col lg="4" className="d-flex align-items-center h-100 row-cel pl-4">
-                    <div>Task 1</div>
+                    <div>{this.props.task.Task}</div>
                 </Col>
                 <Col lg="4" className="d-flex align-items-center h-100 row-cel">
-                    <div>Date</div>
+                    <div>{this.props.task.created_at}</div>
                 </Col>
-                <Col className="d-flex align-items-center h-100">
-                    <Badge color="primary" pill>Normal</Badge>
+                <Col className="d-flex align-items-center justify-content-center h-100">
+                    <Badge color={this.props.task.status==='pending'?'warning':this.props.task.status==='in-progress'?'primary':'success'} pill> {this.props.task.status} </Badge>
                 </Col>
                 <Col></Col>
 
                 <Col className="d-flex align-items-center justify-content-end h-100 row-cel">
-                    <FontAwesomeIcon icon={faFlag} color="red"/>
+                    <FontAwesomeIcon icon={faFlag} color={this.props.task.priority==='normal' ? 'yellowgreen' : this.props.task.priority==='low'?'grey':'red'} />
                 </Col>
                 <Col></Col>
                 <Col className="d-flex align-items-center justify-content-end h-100 row-cel">
@@ -32,12 +39,12 @@ export default class TodoItem extends Component {
                         <FontAwesomeIcon icon={faArrowRight} />
                     </button></Col>
                 <Col className="d-flex align-items-center justify-content-end h-100 row-cel">
-                    <button className="btn btn-header-danger icon-button">
+                    <button className="btn btn-header-danger icon-button" onClick={this.props.onDelete}>
                         <FontAwesomeIcon icon={faTimes} />
                     </button>
                 </Col>
                 <Col className="d-flex align-items-center justify-content-end h-100 row-cel">
-                    <button className="btn btn-header-success icon-button">
+                    <button className="btn btn-header-success icon-button" onClick={this.props.onDone}>
                         <FontAwesomeIcon icon={faCheck} />
                     </button>
                 </Col>
--- src/views/Todo.js
+++ src/views/Todo.js
@@ -15,24 +15,52 @@ import {
 import Select from 'react-select';
 import 'bootstrap/dist/css/bootstrap.css';
 import TodoItem from '../components/TodoItem';
+import TodoItems from '../data/Tdods.json'
 
 export default class Todo extends Component {
-    state = {
-        priorities: [
-            { value: 'urgent', label: 'Urgent' },
-            { value: 'normal', label: 'Normal' },
-            { value: 'low', label: 'Low' }
-        ],
-        priority: ''
+    constructor(props) {
+        super(props)
+        this.onSelectPriority = this.onSelectPriority.bind(this);
+        this.onChangeStatus = this.onChangeStatus.bind(this);
+        this.onDeleteItem = this.onDeleteItem.bind(this);
+        this.state = {
+            priorities: [
+                { value: 'urgent', label: 'Urgent' },
+                { value: 'normal', label: 'Normal' },
+                { value: 'low', label: 'Low' }
+            ],
+            todoItems : [],
+            item: '',
+            priority: '',
+            status: 'pending',
 
+        }
+    }
+
+    componentDidMount(){
+        this.setState({todoItems : TodoItems.data})
     }
 
     onSelectPriority = value => {
         this.setState({ priority: value.value })
 
     }
+    onChangeStatus(id, status){
+        let items = [...this.state.todoItems];
+        let index = items.findIndex(item => item.id === id);
+        console.log("index", index)
+        items[index].status = status;
+        this.setState({todoItems : items})
+    }
+    onDeleteItem(id){
+        let items = [...this.state.todoItems];
+        let index = items.findIndex(item => item.id === id);
+        items.splice(index,1)
+        this.setState({todoItems : items})
+    }
 
     render() {
+        console.log("Todo items", this.state.todoItems)
         console.log('priority', this.state.priority)
         return (
             <Row className='todo-container d-flex justify-content-center'>
@@ -70,10 +98,21 @@ export default class Todo extends Component {
                 </Col>
                 <Col lg="8" xs="12" className="mt-4">
                     <Card className="todo-card" outline color="white">
+                        {this.state.todoItems.map(item => (
+                            <TodoItem 
+                                key={item.id}
+                                task={item}
+                                onPending = {() => this.onChangeStatus(item.id, 'pending')}
+                                onDone = {() => this.onChangeStatus(item.id, 'done')}
+                                onDelete = {() => this.onDeleteItem(item.id)}
+                            />
+                        ))
+
+                        }
+                        {/* <TodoItem />
                         <TodoItem />
                         <TodoItem />
-                        <TodoItem />
-                        <TodoItem />
+                        <TodoItem /> */}
                     </Card>
                 </Col>
             </Row>
